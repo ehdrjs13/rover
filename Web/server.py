29053e66b3afe64cca9f17b503fbac3af51bf4bf
ehdrjs13 <128569780+ehdrjs13@users.noreply.github.com>
2023-12-18 00:03:12 +0900

    Add files via upload

---
+++ Web/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+ 
+@app.route("/pics/<filename>")
+def files(filename):
+    return send_from_directory('detected_image', filename)
+
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
