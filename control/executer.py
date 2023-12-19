5f54e6bd559264b6f726a33dbc338ffc2fd00890
ehdrjs13 <ehdrjs070102@gmail.com>
2023-12-20 02:01:35 +0900

    implement rover control, connection, web controller *except some js visual effects

---
+++ control/executer.py
@@ -0,0 +1,62 @@
+from movement import Movement
+from flask import Flask
+
+ctrl = Movement()
+app = Flask(__name__)
+
+@app.route('/stop/')
+def stop():
+    ctrl.stop()
+
+    return None
+
+@app.route('/forward/')
+def forward():
+    ctrl.forward()
+
+    return None
+@app.route('/rightForward/')
+def rightForward():
+    ctrl.rightForward()
+
+    return None
+
+@app.route('/leftForward/')
+def leftForward():
+    ctrl.leftForward
+
+    return None
+@app.route('/backward/')
+def backward():
+    ctrl.backward
+
+    return None
+
+@app.route('/leftBackward/')
+def leftBackward():
+    ctrl.leftBackward
+
+    return None
+
+@app.route('/rightBackward/')
+def rightBackward():
+    ctrl.rightBackward()
+
+    return None
+
+@app.route('/rightRotate/')
+def rightRotate():
+    ctrl.rightRotate()
+
+    return None
+
+@app.route('/leftRotate/')
+def leftRotate():
+    ctrl.leftRotate()
+
+    return None
+
+
+
+if __name__ == 'main':
+    app.run
---
+++ control/movement.py
@@ -0,0 +1,43 @@
+from gpiozero import Motor
+
+class Movement():
+    def __init__(self) -> None:
+        self.Rmotor = Motor() # 핀 넘버 추가하기
+        self.Lmotor = Motor() #얘도 핀 넘버 추가하기
+        return
+    
+    def forward(self):
+        self.Rmotor.forward(speed=1)
+        self.Lmotor.forward(speed=1)
+    
+    def rightForward(self):
+        self.Rmotor.forward(speed=0.5)
+        self.Lmotor.forward(speed=1)
+
+    def leftForward(self):
+        self.Rmotor.forward(speed=1)
+        self.Lmotor.forward(speed=0.5)   
+    
+    def backward(self):
+        self.Rmotor.backward(speed=1)
+        self.Lmotor.backward(speed=1)
+
+    def rightBackward(self):
+        self.Rmotor.backward(speed=0.5)
+        self.Lmotor.backward(speed=1)
+
+    def leftBackward(self):
+        self.Rmotor.backward(speed=1)
+        self.Lmotor.backward(speed=0.5)
+
+    def rightRotate(self):
+        self.Rmotor.backward(speed=1)
+        self.Lmotor.forward(speed=1)
+
+    def leftRotate(self):
+        self.Lmotor.backward(speed=1)
+        self.Rmotor.forward(speed=1)
+    
+    def stop(self):
+        self.Lmotor.stop()
+        self.Rmotor.stop()
