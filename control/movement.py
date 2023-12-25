ba93e5daf1f2c7cc6a0109471730dcfdfd8750ba
ehdrjs13 <ehdrjs070102@gmail.com>
2023-12-25 23:58:22 +0900

    update executer

--- control/movement.py
+++ control/movement.py
@@ -2,8 +2,8 @@ from gpiozero import Motor
 
 class Movement():
     def __init__(self) -> None:
-        self.Rmotor = Motor() # 핀 넘버 추가하기
-        self.Lmotor = Motor() #얘도 핀 넘버 추가하기
+        self.Rmotor = Motor(5,6) # 핀 넘버 추가하기
+        self.Lmotor = Motor(23,24) #얘도 핀 넘버 추가하기
         return
     
     def forward(self):
